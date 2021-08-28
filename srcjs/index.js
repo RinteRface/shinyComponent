d6e520d1885891bcee072c8d7aff839512d6a519
DivadNojnarg <dgranjon@ymail.com>
2021-08-28 16:39:06 +0200

    Initial commit

---
+++ srcjs/index.js
@@ -0,0 +1,20 @@
+import { message } from './modules/message.js';
+import 'shiny';
+// Import Framework7
+import Framework7 from 'framework7';
+// Import Framework7 Styles
+import 'framework7/framework7-bundle.min.css';
+// import main app component
+
+// Install F7 Components using .use() method on class:
+import Dialog from 'framework7/esm/components/dialog/dialog.js';
+Framework7.use([Dialog]);
+
+import App from './components/app.f7.html';
+
+
+var app = new Framework7({
+  // specify main app component
+  component: App
+})
+

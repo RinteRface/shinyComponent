d8c61a9c660034ae0f3af5fded40614bf9e2b71f
DivadNojnarg <dgranjon@ymail.com>
2021-09-23 08:15:13 +0200

    add header meta tags in UI

--- R/app_ui.R
+++ R/app_ui.R
@@ -2,12 +2,13 @@
 #'
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
+#' @inheritsParam golem_add_external_resources
 #' @import shiny
 #' @noRd
-app_ui <- function(request) {
+app_ui <- function(request, title = NULL) {
   tagList(
     # Leave this function for adding external resources
-    golem_add_external_resources(),
+    golem_add_external_resources(title),
     # Your application UI logic
     tags$body(
       div(id = "app"),
@@ -22,9 +23,10 @@ app_ui <- function(request) {
 #' resources inside the Shiny application.
 #'
 #' @import shiny
+#' @param title App title
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
-golem_add_external_resources <- function(){
+golem_add_external_resources <- function(title){
 
   add_resource_path(
     'www', app_sys('app/www')
@@ -32,6 +34,22 @@ golem_add_external_resources <- function(){
 
   tags$head(
     favicon(),
+    tags$meta(charset = "utf-8"),
+    tags$meta(
+      name = "viewport",
+      content = "width=device-width, initial-scale=1, 
+      maximum-scale=1, minimum-scale=1, user-scalable=no, 
+      viewport-fit=cover"
+    ),
+    tags$meta(
+      name = "apple-mobile-web-app-capable",
+      content = "yes"
+    ),
+    tags$meta(
+      name = "theme-color",
+      content = "#2196f3"
+    ),
+    tags$title(title)
     #bundle_resources(
     #  path = app_sys('app/www'),
     #  app_title = 'shinyFramework7'
